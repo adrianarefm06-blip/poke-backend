210c9500b51638744645ef6cbe5a3bee07c3e6bb
adria <364250@students.esic.edu>
2026-04-29 13:12:56 +0200

    Limpieza de código: eliminados todos los comentarios innecesarios

--- src/main/java/EEDD/ConexionDB.java
+++ src/main/java/EEDD/ConexionDB.java
@@ -5,11 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    // Datos de tu Docker
-    // Si tu puerto es distinto al 3306, cámbialo aquí
     private static final String URL = "jdbc:mysql://localhost:3306/mi_app_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "root"; // <--- Pon tu clave aquí
+    private static final String PASSWORD = "root";
 
     public static Connection obtenerConexion() {
         Connection conexion = null;
